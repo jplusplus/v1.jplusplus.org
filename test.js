4384439bcb0bab14dc20350bfcf0d4ef9f9369e2
Pirhoo <hello@pirhoo.com>
2012-07-21 15:00:21 +0200

    Run

--- test.js
+++ test.js
@@ -1,44 +1,85 @@
 var express = require('express')
 	, phantom = require('phantom')
-			, app = module.exports = express.createServer();
+			, app = null;
 
-app.get('/', function(req, res){
 
-	console.log('Loading a web page');
+/**
+* @author Pirhoo
+*
+* @function
+* @description
+*/
+exports.boot = function(){
 
-	phantom.create( function(ph) {
-	  
-	  ph.createPage( function(page) {
+  // Environement configuration
+  process.env.DATABASE_URL = process.env.DATABASE_URL || "postgres://pirhoo:pirhoo@localhost:5432/jquest_orm";
+  process.env.PORT = process.env.PORT || 3000;
 
-	    var output = "temp.png"
-	    	, url		 = req.query.url ? req.query.url : "about:blank";
+  // Creates Express server
+  app = module.exports = express.createServer();
 
-	    page.set('viewportSize', { width: 1400, height:400} );
-	    page.open(url, function(status) {
+  // Configuration
+  app.configure(function(){
+    
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');   
+    
+    app.use(app.router);
+    app.use(express.static(__dirname + '/public'));
 
-	      
-	      console.log("Opened %s as %s", url, output);
+  });
 
-	      if(status == "success") {
+  app.configure('development', function(){
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+  });
 
-	      	setTimeout(function() {
+  app.configure('production', function(){
+    app.use( express.errorHandler() );
+  });
+  
 
-		      	page.render(output, function() {
-		      		res.sendfile(output);
-					  	page.release();    	      		
-		      	});
+	app.get('/', function(req, res){
 
-	      	}, 2000);
+		console.log('Loading a web page');
 
-	      }
+		phantom.create( function(ph) {
+		  
+		  ph.createPage( function(page) {
 
-	    });
-	  });
+		    var output = "./public/temp.png"
+		    	, url		 = req.query.url ? req.query.url : "about:blank";
+
+		    page.set('viewportSize', { width: 1400, height:400} );
+		    page.open(url, function(status) {
+
+		      
+		      console.log("Opened %s as %s", url, output);
+
+		      if(status == "success") {
+
+		      	setTimeout(function() {
+
+			      	page.render(output, function() {
+			      		res.sendfile(output);
+						  	page.release();    	      		
+			      	});
+
+		      	}, 2000);
+
+		      }
+
+		    });
+		  });
+		});
+
 	});
 
-});
+  return app;
+};
 
 
-app.listen(3030, function(){
+
+
+exports.boot().listen(process.env.PORT, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });

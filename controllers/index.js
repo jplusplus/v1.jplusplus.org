c1fb682b03d2aa05703329d91a1289af9b8bfa93
Pirhoo <hello@pirhoo.com>
2012-08-06 18:05:40 +0200

    Tons of changes

--- controllers/index.js
+++ controllers/index.js
@@ -17,7 +17,7 @@ module.exports = function(app, db, controllers) {
 					 "/stylesheets/vendor/bootstrap-build/bootstrap.min.css"
 					,"/stylesheets/vendor/bootstrap-build/bootstrap-responsive.min.css"
 					,"/stylesheets/vendor/slabtext.css"
-					,"http://fonts.googleapis.com/css?family=Share:400,700|Leckerli+One"
+					,"http://fonts.googleapis.com/css?family=Share:400,700|Leckerli+One|Averia+Sans+Libre:400,700,300,300italic,400italic,700italic"
 					,"/stylesheets/style.less"
 				], 
 				javascripts: [

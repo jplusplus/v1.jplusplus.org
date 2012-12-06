5e69002c4b60a8d1b82af3fab42b18c0e2a53d10
Pierre Romera <hello@pirhoo.com>
2012-12-06 11:48:20 +0100

    Added logo and updatesd fonts

--- controllers/index.js
+++ controllers/index.js
@@ -46,11 +46,9 @@ module.exports =  function(app, db, controllers) {
 				{ 
 					title: 'Journalism++', 
 					stylesheets: [
-						 "/stylesheets/vendor/bootstrap-build/bootstrap.min.css"
-						,"/stylesheets/vendor/bootstrap-build/bootstrap-responsive.min.css"
-						,"/stylesheets/vendor/slabtext.css"
 						,"http://fonts.googleapis.com/css?family=Share:400,700|Leckerli+One|Averia+Sans+Libre:400,700,300,300italic,400italic,700italic"
 						,"/stylesheets/style.css"
+						,"/stylesheets/vendor/slabtext.css"
 					], 
 					javascripts: [
 						  "/javascripts/vendor/bootstrap/bootstrap.min.js"																

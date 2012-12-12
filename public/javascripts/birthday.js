8b45721fdefc8db352fe6eb6d6a13d36848140fd
Pierre Romera <hello@pirhoo.com>
2012-12-12 15:15:07 +0100

    Added keyboard support

--- public/javascripts/birthday.js
+++ public/javascripts/birthday.js
@@ -2,6 +2,24 @@ $(document).ready(function() {
 
   var $carousel = $(".carousel");
 
+  window.keyboardSlide = function(event) {
+
+    if( $carousel.is(":not(.roundabout-holder)") ) return;
+
+    switch(event.keyCode) {
+      
+      case 37: // left
+        $carousel.roundabout("animateToPreviousChild");
+        break;
+
+      case 39: // right
+        $carousel.roundabout("animateToNextChild");
+        break;
+    }
+
+  };
+  
+
   (window.initRoundabout = function() {
 
     var wWidth =  $(window).width();
@@ -12,10 +30,15 @@ $(document).ready(function() {
     // Create the roundabout holder
     } else if( wWidth > 767 && $carousel.is(":not(.roundabout-holder)") ) {
 
-      $carousel.roundabout({ responsive: true });
+      $carousel.roundabout({ responsive: true, minOpacity:0 });
     }
 
   })();
+
+
+  // Naviguation with the keyboard
+  $(document).keydown(window.keyboardSlide);
+
 
   $(window).resize(function() {
     window.initRoundabout();

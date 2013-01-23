47737077fd7b3bdc9edfaa76f4cb045b04da2749
Pierre Romera <hello@pirhoo.com>
2013-01-23 09:56:16 +0100

    Fix bad swedish code

---
+++ locales/sv.js
@@ -0,0 +1,14 @@
+{
+	"Data-driven stories": "Data-driven stories",
+	"Journalism++ is a network of datajournalists that aims to build great stories and tools with data.": "Journalism++ is a network of datajournalists that aims to build great stories and tools with data.",
+	"← Back to the homepage!": "← Back to the homepage!",
+	"Well... It's ambarrassing!": "Well... It's ambarrassing!",
+	"We didn't find the page you are looking for.": "We didn't find the page you are looking for.",
+	"This page is only available in English. We're working on translating it in your language.": "This page is only available in English. We're working on translating it in your language.",
+	"See more": "See more",
+	"Back": "Back",
+	"CEO &bullet; Data-journalist": "CEO &bullet; Data-journalist",
+	"CTO &bullet; Developer": "CTO &bullet; Developer",
+	"Project manager": "Project manager",
+	"Developer": "Developer"
+}

fb3d7b29ec2422c06a1814254b74dd27bd92cf9a
jensfinnas <jens.finnas@gmail.com>
2013-01-23 08:15:37 +0100

    Update locales/se.js

--- locales/se.js
+++ locales/se.js
@@ -1,13 +1,13 @@
 {
 	"Data-driven stories": "Data-driven stories",
-	"CEO &bullet; Data-journalist": "CEO &bullet; Data-journalist",
-	"CTO &bullet; Developer": "CTO &bullet; Developer",
-	"Project manager": "Project manager",
-	"Developer": "Developer",
-	"← Back to the homepage!": "← Back to the homepage!",
-	"Well... It's ambarrassing!": "Well... It's ambarrassing!",
-	"We didn't find the page you are looking for.": "We didn't find the page you are looking for.",
-	"See more": "See more",
-	"Back": "Back",
-	"This page is only available in English. We're working on translating it in your language.": "This page is only available in English. We're working on translating it in your language."
+	"CEO &bullet; Data-journalist": "CEO &bullet; Datajournalist",
+	"CTO &bullet; Developer": "CTO &bullet; Utvecklare",
+	"Project manager": "Projektledare",
+	"Developer": "Utvecklare",
+	"← Back to the homepage!": "← Tillbaka till förstasidan.",
+	"Well... It's ambarrassing!": "Eh... det är lite pinsamt!",
+	"We didn't find the page you are looking for.": "Sidan du sökte hittades inte.",
+	"See more": "Läs mer",
+	"Back": "Tillback",
+	"This page is only available in English. We're working on translating it in your language.": "Den här sidan är inte översatt till svenska ännu."
 }

9160e1dc8dbd45b2673d349365d0bbbc819986aa
Ana Isabel Carvalho <aiscarvalho@users.noreply.github.com>
2014-03-17 17:51:57 +0000

    Update pt.js
    Added the translations.

--- locales/pt.js
+++ locales/pt.js
@@ -1,12 +1,12 @@
 {
-	"Journalism++ is a network of datajournalists that aims to build great stories and tools with data.": "Journalism++ is a network of datajournalists that aims to build great stories and tools with data.",
-	"Data-driven stories": "Data-driven stories",
+	"Journalism++ is a network of datajournalists that aims to build great stories and tools with data.": "Journalism++ é uma rede de jornalismo de dados dedicada à criação de narrativas e ferramentas baseadas em dados.",
+	"Data-driven stories": "Histórias orientadas por dados",
 	"Designer &bullet; Front-end Developer": "Designer &bullet; Front-end Developer",
-	"Designer &bullet; Data architect": "Designer &bullet; Data architect",
-	"Editor": "Editor",
-	"← Back to the homepage!": "← Back to the homepage!",
-	"Well... It's ambarrassing!": "Well... It's ambarrassing!",
-	"We didn't find the page you are looking for.": "We didn't find the page you are looking for.",
-	"See more": "See more",
-	"Back": "Back"
+	"Designer &bullet; Data architect": "Designer &bullet; Arquiteto de dados",
+	"Editor &bullet; Project manager": "Editora &bullet; gestora de projetos",
+	"← Back to the homepage!": "← Voltar ao início!",
+	"Well... It's ambarrassing!": "Oops... Isto é embaraçoso!",
+	"We didn't find the page you are looking for.": "Não encontramos a página que procura.",
+	"See more": "Ver mais",
+	"Back": "Voltar"
 }
